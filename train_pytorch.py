967212360cc10187a96864481ee7d60c8d04862a
Razi <razi.rezaei@tum.de>
2020-01-27 15:47:16 +0100

    Create train_pytorch.py

---
+++ train_pytorch.py
@@ -0,0 +1,16 @@
+import argparse
+import os
+from math import log10
+
+import pandas as pd
+import torch.optim as optim
+import torch.utils.data
+import torchvision.utils as utils
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+import pytorch_ssim
+from data_utils import TrainDatasetFromFolder, ValDatasetFromFolder, display_transform
+from loss import GeneratorLoss
+from model import Generator, Discriminator

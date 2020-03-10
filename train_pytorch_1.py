e7cc2234a3f777026835af812defe67d91e8c942
Razi <razi.rezaei@tum.de>
2020-03-10 16:09:53 +0100

    Update train_pytorch_1.py

--- train_pytorch_1.py
+++ train_pytorch_1.py
@@ -8,18 +8,19 @@ import torch.utils.data
 import torchvision.utils as utils
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
-from tqdm import tqdm
 
+from tqdm import tqdm
 import pytorch_ssim
-from data_utils import TrainDatasetFromFolder, ValDatasetFromFolder, display_transform
-from loss import GeneratorLoss
+#from data_utils import TrainDatasetFromFolder, ValDatasetFromFolder, display_transform
+#from loss import GeneratorLoss
 from model import Generator, Discriminator
 
+
 parser = argparse.ArgumentParser(description='Train Super Resolution Models')
-parser.add_argument('--crop_size', default=88, type=int, help='training images crop size')
-parser.add_argument('--upscale_factor', default=4, type=int, choices=[2, 4, 8],
+#? parser.add_argument('--crop_size', default=88, type=int, help='training images crop size')
+#? parser.add_argument('--upscale_factor', default=4, type=int, choices=[2, 4, 8],
                     help='super resolution upscale factor')
-parser.add_argument('--num_epochs', default=100, type=int, help='train epoch number')
+#? parser.add_argument('--num_epochs', default=100, type=int, help='train epoch number')
 
 
 if __name__ == '__main__':
